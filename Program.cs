73058d23499e46a25688884a66ed71741bf664c1
ilya-re <rezenow75@gmail.com>
2025-06-13 02:06:20 +0300

    Replace Environment.Exit() with return statements; Edit ReadChunk() to output the EOF flag; add comments; write README.md

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace png_parser
 {
 	internal class Program {
 		static readonly byte[] PNG_HEADER;
-		static bool eof;
 		static bool do_print_text = false;
 		static readonly Dictionary<string, string> chunk_table = [];
 		static readonly string exe_path;
 		static Program() {
-			exe_path = Assembly.GetExecutingAssembly().Location;
 			PNG_HEADER = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
-			string json_path = Path.Combine(Path.GetDirectoryName(exe_path), "chunk_table.json");
+			// Read chunks' descriptions from the JSON file
+			exe_path = Assembly.GetExecutingAssembly().Location;
+			string json_path = Path.Combine(Path.GetDirectoryName(exe_path) ?? Path.GetPathRoot(exe_path), "chunk_table.json");
 			using FileStream json = new(json_path, FileMode.OpenOrCreate, FileAccess.Read);
 			var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
 			if (dict != null) { chunk_table = dict; }
@@ -21,12 +21,16 @@ namespace png_parser
 		static uint ReadUInt32BE(BinaryReader reader) {
 			return BinaryPrimitives.ReverseEndianness(reader.ReadUInt32());
 		}
-		static string ReadChunk(FileStream img_file, BinaryReader reader) {
-			uint chunk_length = 0;
+		static string ReadChunk(FileStream img_file, BinaryReader reader, out bool eof_reached) {
+			eof_reached = false;
 			string text_string = string.Empty;
 			// Read chunk length as 32-bit unsigned big-endian integer
+			uint chunk_length;
 			try { chunk_length = ReadUInt32BE(reader); }
-			catch (EndOfStreamException) { eof = true; }
+			catch (EndOfStreamException) {
+				eof_reached = true;
+				return string.Empty;
+			}
 			// Read chunk type as 4 8-bit characters
 			string chunk_type = new(reader.ReadChars(4));
 			bool print_text_chunk = chunk_type == "tEXt" && do_print_text;
@@ -38,20 +42,21 @@ namespace png_parser
 			}
 			// Read chunk CRC as 32-bit unsigned big-endian integer
 			var crc = ReadUInt32BE(reader);
+			// Print chunk info
 			Console.Write($"Chunk \"{chunk_type}\", {chunk_length,-5} bytes, CRC = {crc:X08} ");
-			Console.WriteLine(chunk_table.ContainsKey(chunk_type) ? $"({chunk_table[chunk_type]})" : string.Empty);
+			Console.WriteLine(chunk_table.TryGetValue(chunk_type, out string? chunk_desc) ? $"({chunk_desc})" : string.Empty);
 			// Print text from the tEXt chunk
 			if (print_text_chunk) {
 				var old_color = Console.ForegroundColor;
 				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.WriteLine($"{text_string}");
+				Console.WriteLine(text_string);
 				Console.ForegroundColor = old_color;
 			}
 			return chunk_type;
 		}
-		static void Main(string[] args) {
+		static int Main(string[] args) {
 			if (args.Length == 0) {
-				Console.WriteLine($"Usage: {Path.GetFileNameWithoutExtension(exe_path)} -i <filename> [--print-text]");
+				Console.WriteLine($"Usage: {Path.GetFileName(exe_path)} -i <filename> [--print-text]");
 				Environment.Exit(0);
 			}
 			string? filename = null;
@@ -61,24 +66,30 @@ namespace png_parser
 			}
 			if (filename == null) {
 				Console.WriteLine("No filename specified.");
-				Environment.Exit(1);
+				return 1;
 			}
-			var img_file = new FileStream(filename, FileMode.Open, FileAccess.Read);
-			var reader = new BinaryReader(img_file, System.Text.Encoding.ASCII);
+			using var img_file = new FileStream(filename, FileMode.Open, FileAccess.Read);
+			using var reader = new BinaryReader(img_file, System.Text.Encoding.ASCII);
 			byte[] file_header = reader.ReadBytes(8);
-			if (file_header.Length == 0) {
-				Console.WriteLine("The file is empty.");
-				Environment.Exit(1);
+			string error_msg = string.Empty;
+			// Check if the file is not empty
+			bool file_is_empty = file_header.Length == 0;
+			if (file_is_empty) { error_msg = "The file is empty."; }
+			// Check if the file is PNG
+			bool file_is_not_png = !file_header.SequenceEqual(PNG_HEADER);
+			if (file_is_not_png) { error_msg = "The file does not start with a PNG header."; }
+			// If either check has failed, print the error message and exit
+			if (file_is_empty || file_is_not_png) {
+				Console.WriteLine(error_msg);
+				return 1;
 			}
-			if (!file_header.SequenceEqual(PNG_HEADER)) {
-				Console.WriteLine("The file does not start with a PNG header.");
-				Environment.Exit(1);
+			else {
+				Console.WriteLine("PNG header - OK");
+				string chunk_type;
+				bool eof;
+				do { chunk_type = ReadChunk(img_file, reader, out eof); } while (!(eof || chunk_type == "IEND"));
 			}
-			Console.WriteLine("PNG header - OK");
-			string chunk_type;
-			do { chunk_type = ReadChunk(img_file, reader); } while (!(eof || chunk_type == "IEND"));
-			reader.Close();
-			img_file.Close();
+			return 0;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # png-parser
+
+A command-line utility to display the structure of a PNG file.
+
+Usage: `png-parser -i <PNG file> [--print-text]`
+--print-text: print the tEXt chunk contents
